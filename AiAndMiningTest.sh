87087a0726581da4bfd0df35e903dadb968d93f5
Pukerud <35293441+Pukerud@users.noreply.github.com>
2024-10-17 11:48:57 +0200

    Update AiAndMiningTest.sh

--- AiAndMiningTest.sh
+++ AiAndMiningTest.sh
@@ -10,47 +10,48 @@ if ! command -v screen &> /dev/null; then
 else
     echo "screen is already installed."
 fi
-
-# Kill any previous screen session with the name 'nosana'
-if screen -list | grep -q "nosana"; then
-    echo "Killing existing 'nosana' screen session..."
-    screen -S nosana -X quit
-fi
 # Cleanup
 echo "Cleaning up existing scripts..."
 rm -f "$CURRENT_DIR/AiAndMiningCore.sh" "$CURRENT_DIR/testgrid.sh"
 
-# Start a new screen session and run the AiAndMiningCore.sh script
-echo "Starting a new 'nosana' screen session..."
-wget https://github.com/Pukerud/Nosana/releases/download/test/AiAndMiningCore.sh
-chmod +x AiAndMiningCore.sh
-screen -dmS nosana "$CURRENT_DIR/AiAndMiningCore.sh"
-
-# Check if the screen session started correctly
+# Kill any previous screen session with the name 'nosana'
 if screen -list | grep -q "nosana"; then
-    echo "Screen session 'nosana' is successfully running."
-else
-    echo "Failed to start the 'nosana' screen session."
-fi
-
-# Kill any previous screen session with the name 'nosana2'
-if screen -list | grep -q "nosana2"; then
-    echo "Killing existing 'nosana2' screen session..."
+    echo "Killing existing 'nosana' screen session..."
     screen -S nosana2 -X quit
 fi
 
 # Start a new screen session for the testgrid script
-echo "Starting a new 'nosana2' screen session..."
+echo "Starting a new 'nosana' screen session..."
 wget -O "$CURRENT_DIR/testgrid.sh" https://nosana.io/testgrid.sh
 chmod +x "$CURRENT_DIR/testgrid.sh"
 screen -dmS nosana2 bash "$CURRENT_DIR/testgrid.sh"
 
 # Check if the testgrid screen session started correctly
+if screen -list | grep -q "nosana"; then
+    echo "Screen session 'nosana' is successfully running."
+else
+    echo "Failed to start the 'nosana' screen session."
+fi
+# Kill any previous screen session with the name 'nosana2'
+if screen -list | grep -q "nosana2"; then
+    echo "Killing existing 'nosana2' screen session..."
+    screen -S nosana -X quit
+fi
+
+# Start a new screen session and run the AiAndMiningCore.sh script
+echo "Starting a new 'nosana2' screen session..."
+wget https://github.com/Pukerud/Nosana/releases/download/test/AiAndMiningCore.sh
+chmod +x AiAndMiningCore.sh
+screen -dmS nosana2 "$CURRENT_DIR/AiAndMiningCore.sh"
+
+# Check if the screen session started correctly
 if screen -list | grep -q "nosana2"; then
     echo "Screen session 'nosana2' is successfully running."
 else
     echo "Failed to start the 'nosana2' screen session."
 fi
+
+
 
 echo "Use 'screen -r nosana' to reconnect to the Core session."
 echo "Use 'screen -r nosana2' to reconnect to the testgrid session."
